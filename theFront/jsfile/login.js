4ade05c9faf8aee91a54337f2ff477b1cd490676
EbBlovery <18217052735@163.com>
2017-09-06 23:57:47 +0800

    front

---
+++ theFront/jsfile/login.js
@@ -0,0 +1,9 @@
+$(function(){
+   $('#submit').click(function(){
+   	   var username = $('#username').val();
+   	   var password = $('#password').val();
+   	   if(!username || !password){
+           console.log('qinshurumimahuo zhanghao');
+   	   }
+   })
+})
