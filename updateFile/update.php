99130e62cf90d9266d190d65a571d063a0efbd1d
EbBlovery <18217052735@163.com>
2017-09-09 16:45:14 +0800

    update

--- updateFile/update.php
+++ updateFile/update.php
@@ -2,15 +2,21 @@
     include '../dbcon.php';
 
     // echo $_SERVER['QUERY_STRING'];
-    
-	$str = parse_url_param($_SERVER['QUERY_STRING']);
-    $star = $str['star'];
-    $id = $str['id'];
+    $star = $_POST['star'];
+    $id = $_POST['id'];
+    $product_name = $_POST['product_name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $assort = $_POST['assort'];
 
     // echo $star . $id;
 
-    $sql = $con->query("UPDATE Products SET star = '$star' WHERE id = '$id'");
-    if($sql){
+    $a = $con->query("UPDATE Products SET star = '$star' WHERE id = '$id'");
+    $b = $con->query("UPDATE Products SET product_name = '$product_name' WHERE id = '$id'");
+    $c = $con->query("UPDATE Products SET description = '$description' WHERE id = '$id'");
+    $d = $con->query("UPDATE Products SET price = '$price' WHERE id = '$id'");
+    $e = $con->query("UPDATE Products SET assort = '$assort' WHERE id = '$id'");
+    if($a && $b && $c && $d && $e){
        echo '更新成功！';
     }else{
     	echo 'error :' . $con->error;
